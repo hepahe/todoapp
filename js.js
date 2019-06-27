ca9243e5f01ecb8258f863b2fed75c6a040ec7bc
hepahe <henna.heinajarvi@gmail.com>
2019-06-27 20:38:48 +0300

    added modules, button functionality

--- js.js
+++ js.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+let UIActions = (function() {
+// show unfinished task
+//show finished task
+//show 10 latest finished tasks 
+})();
+
+let dataChanges = (function() {
+//copy input
+//add task
+})();
+
+let events = (function(UIActs, dataChngs) {
+
+    let addBtn = function () {
+    //add eventhandler
+    //delete eventhandler
+    //mark finished eventhandler 
+    }
+
+    document.querySelector('.add-btn').addEventListener('click', addBtn);
+    document.addEventListener('keypress', function(e) {
+        if (e.keyCode === 13 || e.which ===13) {
+            addBtn();
+        }
+    })
+
+})(UIActions, dataChanges);
